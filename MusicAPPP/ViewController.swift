475372f1cd3677497a6be4d56c557a3aed9e1e8f
Ron <onedarksky@icloud.com>
2021-11-02 02:04:27 +0800

    Initial Commit

---
+++ MusicAPPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAPPP
+//
+//  Created by 江庸冊 on 2021/11/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
